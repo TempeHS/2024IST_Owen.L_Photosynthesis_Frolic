fef30af67cd897da28fef6f3b0fc6c8b85cc6eb9
OwenLeigh <owen.leigh@education.nsw.gov.au>
2024-06-04 10:36:15 +1000

    finish enemy1 start on ranged enemy

--- My project/Assets/Scripts/MovingPlatform.cs
+++ My project/Assets/Scripts/MovingPlatform.cs
@@ -7,7 +7,36 @@ public class MovingPlatform : MonoBehaviour
     public Transform TutEnemy;
      public Transform startPoint;
       public Transform endPoint;
+      public float speed = 1.5f;
     
+    int direction = 1;
+
+       private void Update()
+    {
+       Vector2 target = currentMovementTarget();
+
+       TutEnemy.position = Vector2.Lerp(TutEnemy.position, target, speed * Time.deltaTime); 
+
+       float distance = (target - (Vector2)TutEnemy.position).magnitude;
+
+       if(distance <= 0.1f)
+    {
+        direction *= -1;
+    }
+    }
+
+Vector2 currentMovementTarget()
+{
+    if (direction == 1)
+    {
+return startPoint.position;
+    }
+    else
+    {
+        return endPoint.position;
+    }
+}
+   
     private void OnDrawGizmos()
     {
         if(TutEnemy!=null && startPoint!=null && endPoint!=null)
@@ -22,8 +51,5 @@ public class MovingPlatform : MonoBehaviour
     }
 
     
-    void Update()
-    {
-        
-    }
+
 }

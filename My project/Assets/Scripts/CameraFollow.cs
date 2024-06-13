feac8e7074e91ae800f75aee14e71b28b88f5650
OwenLeigh <owen.leigh@education.nsw.gov.au>
2024-06-13 11:54:21 +1000

    Tutorial Finished, Level 1 Started, Level 1 layout done, Level 1 almost finished

--- My project/Assets/Scripts/CameraFollow.cs
+++ My project/Assets/Scripts/CameraFollow.cs
@@ -8,7 +8,6 @@ public class CameraFollow : MonoBehaviour
     public float yOffset =1f;
     public Transform target;
 
-    // Update is called once per frame
     void Update()
     {
         Vector3 newPos = new Vector3(target.position.x,target.position.y + yOffset,-10f);

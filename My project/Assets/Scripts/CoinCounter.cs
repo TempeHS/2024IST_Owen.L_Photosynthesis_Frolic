2a069c026681a42acaeacb7250f168c1c9110865
OwenLeigh <owen.leigh@education.nsw.gov.au>
2024-08-13 10:32:26 +1000

    Level editing, Restarted and completed Coin collectible system

---
+++ My project/Assets/Scripts/CoinCounter.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CoinCounter : MonoBehaviour
+{
+    public static CoinCounter instance;
+
+    public TMP_Text coinText;
+    public int currentCoins = 0;
+    // Start is called before the first frame update
+   void Awake()
+   {
+    instance = this;
+   }
+   
+    void Start()
+    {
+        coinText.text = "COINS: " + currentCoins.ToString();
+    }
+
+    // Update is called once per frame
+    public void IncreaseCoins(int v)
+    {
+        currentCoins += v;
+        coinText.text = "COINS: " + currentCoins.ToString();
+    }
+}
--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -8,7 +8,6 @@ public class PlayerMovement : MonoBehaviour
     private float speed = 8f;
     private float jumpingPower = 24f;
     private bool isFacingRight = true;
-    public CoinManager cm;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
@@ -52,12 +51,5 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    void OnTriggerEnter2d(Collider2D other)
-    {
-        if(other.gameObject.CompareTag("Coin"))
-        {
-            cm.coinCount++;
-        }
-
-}
+   
 }

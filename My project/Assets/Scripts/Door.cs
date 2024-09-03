8e392fec8b93af59d946f0b7b181352cbaf91593
OwenLeigh <owen.leigh@education.nsw.gov.au>
2024-09-03 11:55:16 +1000

    door type shii

--- My project/Assets/Scripts/Door.cs
+++ My project/Assets/Scripts/Door.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform door;
+     public Transform startPoint;
+      public Transform endPoint;
+     public float speed = 5f;
+    
+    int direction = 1;
+
+      private void Update()
+    {
+       Vector2 target = currentMovementTarget();
+
+       door.position = Vector2.Lerp(door.position, target, speed * Time.deltaTime); 
+
+       float distance = (target - (Vector2)door.position).magnitude;
+
+       if(distance <= 0.1f)
+    {
+        direction *= -1;
+    }
+    }
+
+Vector2 currentMovementTarget()
+{
+    if (direction == 1)
+    {
+return startPoint.position;
+    }
+    else
+    {
+        return endPoint.position;
+    }
+}
+   
+    private void OnDrawGizmos()
+    {
+        if(door!=null && startPoint!=null && endPoint!=null)
+        {
+            Gizmos.DrawLine(door.transform.position,startPoint.position);
+            Gizmos.DrawLine(door.transform.position,endPoint.position);
+        }
+    }
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
+
 }
+
